42519548dee631a8c196e11a7fd79fb3707d13b1
Isaiah Montoya <idmontoya@yahoo.com>
2021-02-24 16:35:11 -0900

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,4 +1,83 @@
 # Use this space to code along with the readme
+phrase = "Hello There!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+
+# ... the rest of the program
+
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+
+def say_hello_there_ten_times
+    phrase = "Hello There!"
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+end
+def say_hello_there_ten_times
+    phrase = "Hello There!"
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+end 
+say_hello_there_ten_times
+
+# ... The rest of the program
+
+say_hello_there_ten_times
+
+def greeting # Method Signature
+    puts "Hello There" # Method Body
+end # Method Closing
+def greeting
+    # Leave a line break for the method body
+end # Immediately close the method.
+
+def greeting
+    puts "Hello There" # Now code the body of the method.
+end
+
+def greeting
+    puts "Hello There"
+end
+
+greeting # Executing the method by name
+#=> "Hello There"
+
+greeting # Executing the method again
+#=> "Hello There"
 
 phrase = "Hello World!"
 puts phrase
